2d1e5a39ba53ca357caf79da0327655b66959a5a
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-09-04 18:57:44 -0300

    Created supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://ulvxokhhsawattxgwdlk.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsdnhva2hoc2F3YXR0eGd3ZGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MDk2OTUsImV4cCI6MjA0MDk4NTY5NX0.m5RHN5WryNqTDW4NrkfuOyeq94iyPkgK3gHY3zJBdmc"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
