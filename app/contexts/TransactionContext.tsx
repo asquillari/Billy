50ec9c774e7cd5d6ec5601e6ba6d8c1dd84ac703
Matías Leporini <matilepo03@gmail.com>
2024-09-30 09:10:10 -0300

    small

--- app/contexts/TransactionContext.tsx
+++ app/contexts/TransactionContext.tsx
@@ -31,8 +31,6 @@ export const TransactionProvider: React.FC<{ children: React.ReactNode }> = ({ c
 
 export const useTransactions = () => {
   const context = useContext(TransactionContext);
-  if (context === undefined) {
-    throw new Error('useTransactions must be used within a TransactionProvider');
-  }
+  if (context === undefined) throw new Error('useTransactions must be used within a TransactionProvider');
   return context;
 };
