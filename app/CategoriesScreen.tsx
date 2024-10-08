174455c8ca83bf6d1d8d3cc2aea435276ae89289
Matías Leporini <63206927+LosProgramasdeLepo@users.noreply.github.com>
2024-10-08 13:31:58 -0300

    Closer...

--- app/CategoriesScreen.tsx
+++ app/CategoriesScreen.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { CategoryData } from '@/api/api';
 import { useCategoryData } from '@/hooks/useCategoryData'; 
+import { BillyHeader } from '@/components/BillyHeader';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const CategoriesScreen = () => {
     const { categoryData } = useCategoryData();
+
+    const sortedCategories = useMemo(() => {
+        if (!categoryData) return [];
+        const othersCategory = categoryData.find(cat => cat.name === "Otros");
+        const otherCategories = categoryData.filter(cat => cat.name !== "Otros").sort((a, b) => parseFloat(String(b.spent ?? '0')) - parseFloat(String(a.spent ?? '0')));
+        return [...otherCategories, othersCategory];
+    }, [categoryData]);
 
     const renderCategory = ({ item }: { item: CategoryData }) => (
         <View style={styles.categoryItem}>
@@ -15,12 +24,17 @@ const CategoriesScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Categories</Text>
-            <FlatList
-                data={categoryData}
-                renderItem={renderCategory}
-                keyExtractor={(item) => item.id || 'defaultKey'}
-            />
+            <LinearGradient colors={['#4B00B8', '#20014E']} style={styles.gradientContainer}>
+                <BillyHeader/>
+                <View style={styles.contentContainer}>
+                    <Text style={styles.title}>Categorías</Text>
+                    <FlatList
+                        data={categoryData}
+                        renderItem={renderCategory}
+                        keyExtractor={(item) => item.id || 'defaultKey'}
+                    />
+                </View>
+            </LinearGradient>
         </View>
     );
 };
@@ -28,12 +42,24 @@ const CategoriesScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
+    },
+    gradientContainer: {
+        flex: 1,
+    },
+    contentContainer: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        marginTop: 10,
+        marginHorizontal: '2.5%',
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 16,
+        alignSelf: 'center',
+        marginTop: 10,
     },
     categoryItem: {
         padding: 10,
